3fc2b1189f319201e69c9745be99519ba982ece2
nooblisk <mitarevigor@gmail.com>
2017-09-21 13:43:27 +0300

    some improvements

--- app/actions/app.js
+++ app/actions/app.js
@@ -9,12 +9,12 @@ const getAuthorizationHeaders = () => ({
 	Authorization: `Client-ID 5b45f3fd333af6f`,
 });
 
-export function getGallery() {
+export function getGallery(dateRange) {
 	return {
 		ajaxAction: true,
 		method: 'get',
 		name: GET_GALLERY,
-		url: `${BASE_URL}/3/gallery/top/top/`,
+		url: `${BASE_URL}/3/gallery/top/top/${dateRange}`,
 		headers: getAuthorizationHeaders(),
 		payload: {},
 	};
--- app/containers/Gallery.js
+++ app/containers/Gallery.js
@@ -7,22 +7,26 @@ import {
 	getGallery,
 } from 'actions/app';
 
+const DATE_RANGE = 'day';
+
 class Gallery extends Component {
 	static fetchData({ store }) {
-		return store.dispatch(getGallery());
+		return store.dispatch(getGallery(DATE_RANGE));
 	}
+
 	// componentDidMount() {
-	// 	this.props.getGallery();
+	// 	this.props.getGallery(DATE_RANGE);
 	// }
 
 	render() {
 		console.log('this.props.app.images = ', this.props.app.images); // TODO Remove
 		const { images } = this.props.app;
+		const filteredImages = images.filter(image => image.type !== undefined);
 		return (
 			<div>
-				<p>privet ya gallery</p>
+				<h1>{`Imgur top images of the ${DATE_RANGE}`}</h1>
 				{
-					images.map(image => <ImageCard key={`ImageCard_${image.id}`} image={image} />)
+					filteredImages.map(image => <ImageCard key={`ImageCard_${image.id}`} image={image} />)
 				}
 			</div>
 		);
